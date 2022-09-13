189a3a4f952a55a98aaba0d03ee3de5b43eddf0a
Kim Jung <godrm77@gmail.com>
2022-09-13 15:51:30 +0900

    참여할 사람 입력 처리

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -7,5 +7,13 @@
 
 import Foundation
 
-print("Hello, World!")
+print("참여할 사람은 몇 명인가요?")
+var numberOfplayer = readLine() ?? ""
+var numbers = Int(numberOfplayer) ?? 0
 
+if numbers < 2 || numbers > 8 {
+    print("적절하지 않아서 종료합니다")
+    exit(1)
+}
+
+print("계속 진행합니다")
